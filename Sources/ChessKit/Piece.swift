960f7e7c6dd4b35b1fada3642ad6793bc85ef6ea
Alexander Perechnev <aperechnev@icloud.com>
2020-07-12 19:15:23 +0200

    Piece refactoring.

--- Sources/ChessKit/Piece.swift
+++ Sources/ChessKit/Piece.swift
@@ -7,13 +7,22 @@
 //
 
 /// Piece on `Board` with it's color.
-public struct Piece: Equatable {
+public struct Piece: Equatable, CustomStringConvertible {
+    
+    private static let kindToCharacterMap: [PieceKind:Character] = [
+        .king: "k", .queen: "q", .rook: "r", .bishop: "b", .knight: "n", .pawn: "p"
+    ]
+    private static let characterToKindMap: [String:PieceKind] = [
+        "k": .king, "q": .queen, "r": .rook, "b": .bishop, "n": .knight, "p": .pawn
+    ]
     
     /// Piece kind.
     public let kind: PieceKind
     
     /// Piece color.
     public let color: PieceColor
+    
+    // MARK: Initialization
     
     /**
      Initializes a piece with it's kind and color.
@@ -27,36 +36,33 @@ public struct Piece: Equatable {
         self.color = color
     }
     
+    /**
+     Initializes a piece with given character.
+     
+     Possible values are: `K` for white king, `n` for black pawn and so on.
+     
+     - Parameters:
+        - character: Character that represents a piece.
+     */
     public init?(character: Character) {
-        let color: PieceColor = character.isUppercase ? .white : .black
-        let kind: PieceKind
-        
-        switch character.lowercased() {
-        case "k": kind = .king
-        case "q": kind = .queen
-        case "r": kind = .rook
-        case "b": kind = .bishop
-        case "n": kind = .knight
-        case "p": kind = .pawn
-        default: return nil
+        guard let kind: PieceKind = Piece.characterToKindMap[character.lowercased()] else {
+            return nil
         }
-        
+        let color: PieceColor = character.isUppercase ? .white : .black
         self.init(kind: kind, color: color)
     }
     
-}
-
-extension Piece: CustomStringConvertible {
+    // MARK: Custom string convertable
     
+    /**
+     String that indicates the stored piece.
+     
+     Possible values are: `K` for white king, `r` for black rook and so on.
+     */
     public var description: String {
-        let map: [PieceKind:Character] = [
-            .king: "k", .queen: "q", .rook: "r", .bishop: "b", .knight: "n", .pawn: "p"
-        ]
-        
-        guard let character = map[self.kind] else {
+        guard let character = Piece.kindToCharacterMap[self.kind] else {
             return "<Error>"
         }
-        
         return self.color == .white ? character.uppercased() : character.lowercased()
     }
     
