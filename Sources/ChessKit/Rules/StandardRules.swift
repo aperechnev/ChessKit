c5d9b0cff41def6db505bf51b209cd8ab01b5130
Alexander Perechnev <aperechnev@icloud.com>
2020-07-13 02:49:16 +0200

    Pawn moves.

--- Sources/ChessKit/Rules/StandardRules.swift
+++ Sources/ChessKit/Rules/StandardRules.swift
@@ -17,6 +17,9 @@ public class StandardRules: Rules {
     private let knightTranslations = [
         (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)
     ]
+    private let pawnTakingTranslations = [
+        (-1, 1), (1, 1)
+    ]
     
     public func movesForPiece(at square: Square, in position: Position) -> [String] {
         guard let piece = position.board[square] else {
@@ -72,8 +75,61 @@ public class StandardRules: Rules {
     }
     
     private func pawnMoves(at square: Square, in position: Position) -> [String] {
+        let direction = position.turn == .white ? 1 : -1
+        
         var destinations = [Square]()
-        return destinations.map { "\(square)\($0)" }
+        
+        // One-square move
+        let oneSquareDestination = square.translate(file: 0, rank: direction)
+        if oneSquareDestination.isValid {
+            if position.board[oneSquareDestination] == nil {
+                destinations.append(oneSquareDestination)
+            }
+        }
+        
+        // Two-square moves
+        if position.turn == .white && square.rank == 1 {
+            destinations.append(square.translate(file: 0, rank: 2))
+        }
+        if position.turn == .black && square.rank == 6 {
+            destinations.append(square.translate(file: 0, rank: -2))
+        }
+        
+        // Piece taking
+        for translation in self.pawnTakingTranslations {
+            let takingSquare = square.translate(file: translation.0, rank: translation.1 * direction)
+            if !takingSquare.isValid {
+                continue
+            }
+            if position.board[takingSquare]?.color.negotiated == position.turn {
+                destinations.append(takingSquare)
+            }
+        }
+        
+        // En passant
+        if let enPasantSquare = position.enPasant {
+            for takingTranslation in self.pawnTakingTranslations {
+                if square.translate(file: takingTranslation.0, rank: takingTranslation.1 * direction) == enPasantSquare {
+                    destinations.append(enPasantSquare)
+                }
+            }
+        }
+        
+        // Promotion
+        let promotionRank = position.turn == .white ? 7 : 0
+        let promotions = destinations.filter { $0.rank == promotionRank }
+        destinations = destinations.filter { $0.rank != promotionRank }
+        var promotionMoves = promotions.flatMap {
+            ["\(square)\($0)Q",
+             "\(square)\($0)R",
+             "\(square)\($0)B",
+             "\(square)\($0)N"]
+        }
+        if position.turn == .black {
+            promotionMoves = promotionMoves.map { $0.lowercased() }
+        }
+        
+        return destinations.map { "\(square)\($0)" } + promotionMoves
     }
     
     private func longRangeMoves(at square: Square,
---
+++ Tests/ChessKitTests/Rules/Standard/PawnMovesTests.swift
@@ -0,0 +1,64 @@
+//
+//  PawnMovesTests.swift
+//  ChessKitTests
+//
+//  Created by Alexander Perechnev on 13.07.2020.
+//  Copyright © 2020 Päike Mikrosüsteemid OÜ. All rights reserved.
+//
+
+import XCTest
+@testable import ChessKit
+
+class PawnMovesTests: XCTestCase {
+    
+    func testAlone() throws {
+        self.assert(fen: "8/8/8/8/8/8/4P3/8 w - - 0 1", moves: "e2e3 e2e4", coordinate: "e2")
+        self.assert(fen: "8/5p2/8/8/8/8/8/8 b - - 0 1", moves: "f7f6 f7f5", coordinate: "f7")
+        self.assert(fen: "8/8/8/4P3/8/8/8/8 w - - 0 1", moves: "e5e6", coordinate: "e5")
+        self.assert(fen: "8/8/8/2p5/8/8/8/8 b - - 0 1", moves: "c5c4", coordinate: "c5")
+    }
+    
+    func testPieceTaking() throws {
+        self.assert(fen: "8/8/8/8/3n4/5n2/4P3/8 w - - 0 1", moves: "e2e3 e2e4 e2f3", coordinate: "e2")
+        self.assert(fen: "8/8/5n2/4P3/8/8/8/8 w - - 0 1", moves: "e5e6 e5f6", coordinate: "e5")
+        self.assert(fen: "8/p7/1N6/8/8/8/8/8 b - - 0 1", moves: "a7a6 a7a5 a7b6", coordinate: "a7")
+        self.assert(fen: "8/8/8/8/8/3p4/4N3/8 b - - 0 1", moves: "d3d2 d3e2", coordinate: "d3")
+    }
+    
+    func testPromotion() throws {
+        self.assert(fen: "3n4/4P3/8/8/8/8/8/8 w - - 0 1",
+                    moves: "e7d8Q e7d8R e7d8B e7d8N e7e8Q e7e8R e7e8B e7e8N",
+                    coordinate: "e7")
+        
+        self.assert(fen: "3nr3/4P3/8/8/8/8/8/8 w - - 0 1",
+                    moves: "e7d8Q e7d8R e7d8B e7d8N",
+                    coordinate: "e7")
+        
+        self.assert(fen: "8/8/8/8/8/8/2p5/3B4 b - - 0 1",
+                    moves: "c2c1q c2c1r c2c1b c2c1n c2d1q c2d1r c2d1b c2d1n",
+                    coordinate: "c2")
+        
+        self.assert(fen: "8/8/8/8/8/8/2p5/2RB4 b - - 0 1",
+                    moves: "c2d1q c2d1r c2d1b c2d1n",
+                    coordinate: "c2")
+    }
+    
+    func testEnPassant() throws {
+        self.assert(fen: "rnbqkbnr/pp2pppp/2p5/3pP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3",
+                    moves: "e5e6 e5d6",
+                    coordinate: "e5")
+        
+        self.assert(fen: "rnbqkbnr/ppp1pppp/8/8/3pP1P1/7P/PPPP1P2/RNBQKBNR b KQkq e3 0 3",
+                    moves: "d4d3 d4e3",
+                    coordinate: "d4")
+    }
+    
+    private func assert(fen: String, moves: String, coordinate: String) {
+        let position = FenSerialization.default.deserialize(fen: fen)
+        let square = Square(coordinate: coordinate)
+        let moves = moves.split(separator: " ").map { "\($0)" }
+        let testMoves = StandardRules().movesForPiece(at: square, in: position)
+        XCTAssertEqual(moves.sorted(), testMoves.sorted(), "Position: \(fen)")
+    }
+
+}
