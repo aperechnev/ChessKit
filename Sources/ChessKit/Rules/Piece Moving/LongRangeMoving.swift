57634f8c31d23851148e6ab06cf3cdb15b43eb52
Alexander Perechnev <aperechnev@icloud.com>
2020-07-13 15:46:40 +0200

    Long-range moving refactoring.

--- Sources/ChessKit/Rules/Piece Moving/LongRangeMoving.swift
+++ Sources/ChessKit/Rules/Piece Moving/LongRangeMoving.swift
@@ -15,27 +15,31 @@ class LongRangeMoving: PieceMoving {
     }
     
     func moves(from square: Square, in position: Position) -> [String] {
+        self.translations
+            .flatMap { self.process(translation: $0, for: square, in: position) }
+            .map { "\(square)\($0)" }
+    }
+    
+    private func process(translation: (Int, Int), for square: Square, in position: Position) -> [String] {
         var destinations = [String]()
         
-        for translation in self.translations {
-            for offset in 1..<8 {
-                let destination = square.translate(file: translation.0 * offset, rank: translation.1 * offset)
-                if !destination.isValid {
-                    break
-                }
-                
-                if position.board[destination]?.color == position.turn {
-                    break
-                }
-                if position.board[destination]?.color != nil && position.board[destination]?.color != position.turn {
-                    destinations.append("\(destination)")
-                    break
-                }
-                destinations.append("\(destination)")
+        for offset in 1..<8 {
+            let destination = square.translate(file: translation.0 * offset, rank: translation.1 * offset)
+            if !destination.isValid {
+                break
             }
+            
+            if position.board[destination]?.color == position.turn {
+                break
+            }
+            if position.board[destination]?.color != nil && position.board[destination]?.color != position.turn {
+                destinations.append("\(destination)")
+                break
+            }
+            destinations.append("\(destination)")
         }
         
-        return destinations.map { "\(square)\($0)" }
+        return destinations
     }
     
 }
