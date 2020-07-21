10d6844622e0d29d6c7625d8e9355875806fa7d0
Alexander Perechnev <aperechnev@icloud.com>
2020-07-21 03:00:22 +0200

    Position is a struct and doesn't need deepCopy() method.

--- Tests/ChessKitTests/PositionTests.swift
+++ Tests/ChessKitTests/PositionTests.swift
@@ -15,7 +15,7 @@ class PositionTests: XCTestCase {
         let initialFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         let position = FenSerialization.default.deserialize(fen: initialFen)
         
-        var positionCopy = position.deepCopy()
+        var positionCopy = position
         positionCopy.board["e4"] = nil
         positionCopy.state.castlings = []
         positionCopy.state.enPasant = Square(coordinate: "e4")
