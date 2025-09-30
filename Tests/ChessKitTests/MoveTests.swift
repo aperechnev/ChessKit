c6136db9d92d293a2abf84689353d9e4298c7a6f
Alexander Perechnev <alex.perechnev@gmail.com>
2025-09-30 03:34:59 +0300

    MoveTests

--- Tests/ChessKitTests/MoveTests.swift
+++ Tests/ChessKitTests/MoveTests.swift
@@ -3,29 +3,28 @@
 //  ChessKitTests
 //
 //  Created by Alexander Perechnev on 13.07.2020.
-//  Copyright © 2020 Päike Mikrosüsteemid OÜ. All rights reserved.
+//  Modified by Alexander Perechnev on 30.09.2025.
+//  Copyright © 2020-2025 Päike Mikrosüsteemid OÜ. All rights reserved.
 //
 
-import XCTest
+import Testing
+
 @testable import ChessKit
 
-class MoveTests: XCTestCase {
-    
-    func testInitWithSquares() throws {
-        XCTAssertEqual(
-            Move(from: Square(coordinate: "e2"), to: Square(coordinate: "e4")).description,
-            "e2e4"
-        )
-        
-        XCTAssertEqual(
-            Move(from: Square(coordinate: "d7"), to: Square(coordinate: "d8"), promotion: PieceKind.queen).description,
-            "d7d8q"
-        )
-    }
-    
-    func testInitWithString() throws {
-        XCTAssertEqual(Move(string: "e2e4").description, "e2e4")
-        XCTAssertEqual(Move(string: "f7f8r").description, "f7f8r")
-    }
+@Test func initWithSquares() {
+    #expect(
+        Move(from: Square(coordinate: "e2"), to: Square(coordinate: "e4")).description == "e2e4"
+    )
 
+    #expect(
+        Move(
+            from: Square(coordinate: "d7"), to: Square(coordinate: "d8"),
+            promotion: PieceKind.queen
+        ).description == "d7d8q"
+    )
+}
+
+@Test func initWithString() {
+    #expect(Move(string: "e2e4").description == "e2e4")
+    #expect(Move(string: "f7f8r").description == "f7f8r")
 }
