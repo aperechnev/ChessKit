39f5d1d64e0b79467ee040138b5b2c2cc242a831
Alexander Perechnev <alex.perechnev@gmail.com>
2025-09-30 03:26:37 +0300

    PositionTests

--- Tests/ChessKitTests/PositionTests.swift
+++ Tests/ChessKitTests/PositionTests.swift
@@ -3,27 +3,25 @@
 //  ChessKitTests
 //
 //  Created by Alexander Perechnev on 13.07.2020.
-//  Copyright © 2020 Päike Mikrosüsteemid OÜ. All rights reserved.
+//  Modified by Alexander Perechnev on 30.09.2025.
+//  Copyright © 2020-2025 Päike Mikrosüsteemid OÜ. All rights reserved.
 //
 
-import XCTest
+import Testing
+
 @testable import ChessKit
 
-class PositionTests: XCTestCase {
-    
-    func testDeepCopying() throws {
-        let initialFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-        let position = FenSerialization.default.deserialize(fen: initialFen)
-        
-        var positionCopy = position
-        positionCopy.board["e4"] = nil
-        positionCopy.state.castlings = []
-        positionCopy.state.enPasant = Square(coordinate: "e4")
-        positionCopy.state.turn = .black
-        positionCopy.counter.fullMoves = 100
-        positionCopy.counter.halfMoves = 200
-        
-        XCTAssertEqual(FenSerialization.default.serialize(position: position), initialFen)
-    }
+@Test func deepCopying() {
+    let initialFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    let position = FenSerialization.default.deserialize(fen: initialFen)
 
+    var positionCopy = position
+    positionCopy.board["e4"] = nil
+    positionCopy.state.castlings = []
+    positionCopy.state.enPasant = Square(coordinate: "e4")
+    positionCopy.state.turn = .black
+    positionCopy.counter.fullMoves = 100
+    positionCopy.counter.halfMoves = 200
+
+    #expect(FenSerialization.default.serialize(position: position) == initialFen)
 }
