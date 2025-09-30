b1459f940fb3d7edcf4c3b5cd536179def3d6b15
Alexander Perechnev <alex.perechnev@gmail.com>
2025-09-30 17:06:21 +0300

    Fix for the docs base path.

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,1 +1,1 @@
-swift package --allow-writing-to-directory ./docs generate-documentation --target ChessKit --output-path ./docs --transform-for-static-hosting
+swift package --allow-writing-to-directory ./docs generate-documentation --target ChessKit --output-path ./docs --transform-for-static-hosting --hosting-base-path ChessKit
